dfbf3809270a0187efb002f0b13563775034fde6
SEXYJungmin <jmdev@kakao.com>
2020-07-19 23:59:25 +0900

    test

---
+++ DOM_가지고_놀기/test.js
@@ -0,0 +1,10 @@
+/* const arrowAdd = (num1, num2) => num1 + num2;
+console.log(arrowAdd(3, 35)); */
+/* 
+const arrowPow = num1 => num1*num1;
+console.log(arrowPow); */
+/* 
+const isPot = num => (num < 0 ? 0 : num);
+console.log(isPot(-1)); */
+
+setTimeout(() => console.log("Hello"), 3000);
