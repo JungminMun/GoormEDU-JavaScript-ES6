12145a42d6da426e5d83099bad84d40088011137
SEXYJungmin <jmdev@kakao.com>
2020-07-22 00:25:54 +0900

    2020/07/22 : 배열 가지고 놀기_2. 요소 추가

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
 const numarr = [1, 2, 3, 4];
-// console.log(typeof numarr);
+/* // console.log(typeof numarr);
 
 //배열의 길이 알아보기 : .length
 // console.log(numarr.length);
 const arr = [];
 arr[2] = 3;
-console.log(arr);
+console.log(arr); */
+// 원본 배열을 직접 건드리는 방식
+numarr.push(5, 6);
+// console.log(numarr); 
+
+// 원본 내열을 직접 건드리지 않는 방식
+/* const result = numarr.concat(7, 8);
+console.log(result);
+console.log(numarr);
+ */
+
+ numarr.pop();
+ console.log(numarr);
