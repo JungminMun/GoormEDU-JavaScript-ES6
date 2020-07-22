33e8d0c433d9cb9788f5a5ddf949c232f0e33142
SEXYJungmin <jmdev@kakao.com>
2020-07-22 23:36:58 +0900

    2020/07/22 : 배열 가지고 놀기_4. 필터링, 마무리

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const namearr = ['민철', '영수', '영희', '민수'];
+/* const namearr = ['민철', '영수', '영희', '민수'];
 
 //foreach
 namearr.forEach (name => console.log(`내 이름은 ${name}이야`));
@@ -6,3 +6,14 @@ namearr.forEach (name => console.log(`내 이름은 ${name}이야`));
 const oddarr = [1, 3, 5, 7, 9];
 const newarr = oddarr.map( num => num*2 );
 console.log(newarr);
+
+const filterarr = oddarr.filter(x => x > 4);
+console.log(filterarr); */
+
+const postlist = [
+    {"date" : "yestarday", "id" : 1},
+    {"date" : "yestarday", "id" : 2},
+    {"date" : "today", "id" : 3}];
+
+const yestardaypost = postlist.filter(x => x.date === "yestarday");
+console.log(yestardaypost);
